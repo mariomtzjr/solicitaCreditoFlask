e2130d6121a2343e0fb11458a74f3061e901be1f
mariomtz <>
2019-05-22 10:59:58 -0500

    Initial commit
    - Created core file for app.
    - Created styles file for app.
    - Created login template for app.

---
+++ core.py
@@ -0,0 +1,45 @@
+import os
+from flask import (
+    Flask,
+    flash,
+    redirect,
+    render_template,
+    request,
+    session,
+    abort
+)
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return "Iniciaste Sesión"
+
+
+@app.route('/login', methods=['POST'])
+def admin_login():
+    if request.form['password'] == 'password' and request.form['username'] == 'admin':
+        session['logged_in'] = True
+    else:
+        flash('¡Usuario/Contraseña incorrectos!')
+        return index()
+
+
+@app.route("/clientes")
+def listaCliente():
+    return "Listado de clientes"
+
+
+@app.route("/creditos")
+def listaCreditos():
+    return "Listado de créditos"
+
+
+if __name__ == '__main__':
+    app.secret_key = os.urandom(12)
+    app.run(debug=True, host="0.0.0.0", port=8000)
