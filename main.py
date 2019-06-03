d4649ba4979b9ae80da1c80cef48c2524e36dc93
Mario Martinez <mario.martinez@maskota.com.mx>
2019-06-03 11:50:12 -0500

    Modified form styles

--- main.py
+++ main.py
@@ -97,6 +97,6 @@ if __name__ == '__main__':
     csrf.init_app(app)
 
     db.init_app(app)
-    with app.app_context():
-        db.create_all()
+    # with app.app_context():
+    #     db.create_all()
     app.run(host="0.0.0.0", port=8000)
